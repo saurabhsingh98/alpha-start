d5905618af00939ae9f0f90a366ae2cd1df7eb33
Saurabh Singh <dashingsaurabh1996@gmail.com>
2025-10-18 13:04:26 +0530

    Added react router dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-// import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './components/pages/login';
 
 function App() {
-
   return (
-    <>
-      <div>HEELLO</div>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<>NOT FOUND</>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
