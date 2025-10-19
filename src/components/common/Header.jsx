4b97663227984fc71184ebea1ca00f7205faf008
Saurabh Singh <dashingsaurabh1996@gmail.com>
2025-10-19 11:13:02 +0530

    Added tabs in headers

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -18,8 +18,13 @@ const Header = () => {
         fontSize: '16px',
       }}
     >
-      <div>Alpha Start</div>
+      <div style={{fontFamily:'sans-serif', color: '#30baaf9e'}}>Alpha Start</div>
+      <div style={{fontFamily:'sans-serif', color: '#30baaf9e'}}>
+        <input type="search" name="" id="" />
+      </div>
       <div>Jobs</div>
+      <div>Connections</div>
+      <div>Messaging</div>
       <div>Notification</div>
       <div>User</div>
     </div>
